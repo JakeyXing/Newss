a28b07893d3163bd000b0c7b815448e2338dab4b
JakeyXing <xingjiehai@gmail.com>
2014-08-13 16:23:17 +0800

    first comit

---
+++ Newss/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Newss
+//
+//  Created by xingJH on 14-8-13.
+//  Copyright (c) 2014年 Janlent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end

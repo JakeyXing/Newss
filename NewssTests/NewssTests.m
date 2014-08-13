b77aac622539cea5d2e77d393e2ed11a33927839
JakeyXing <xingjiehai@gmail.com>
2014-08-13 16:00:23 +0800

    Initial Commit

---
+++ NewssTests/NewssTests.m
@@ -0,0 +1,34 @@
+//
+//  NewssTests.m
+//  NewssTests
+//
+//  Created by xingJH on 14-8-13.
+//  Copyright (c) 2014年 Janlent. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NewssTests : XCTestCase
+
+@end
+
+@implementation NewssTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
